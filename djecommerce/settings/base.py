0b19fb4bd486ee93c4c8bc95c8728428382fb02c
Bekkhan <87779571856b@gmail.com>
2022-08-08 23:19:04 +0600

    base

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -1,6 +1,7 @@
 import os
 from decouple import config
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
